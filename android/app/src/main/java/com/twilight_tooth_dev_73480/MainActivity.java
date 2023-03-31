2e196ae58432592f1db19441f25acc2792230a18
Crowdbotics <engineering@crowdbotics.com>
2023-03-31 08:51:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/twilight_tooth_dev_73480/MainActivity.java
@@ -0,0 +1,15 @@
+package com.twilight_tooth_dev_73480;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "twilight_tooth_dev_73480";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-tooth-dev-73480",
+  url: "https://twilight-tooth-dev-73480.botics.co",
+  api: "https://twilight-tooth-dev-73480.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
